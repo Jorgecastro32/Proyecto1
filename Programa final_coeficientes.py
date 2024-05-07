27ae55cd6c13680dceadc617ecab91f371c0f99c
fernandoviolero <167028673+fernandoviolero@users.noreply.github.com>
2024-05-07 13:55:41 +0200

    Add files via upload

--- Programa final_coeficientes.py
+++ Programa final_coeficientes.py
@@ -6,11 +6,12 @@ from pulp import *
 datos = pd.DataFrame({
     'camas': [966, 1140, 1300, 892],   
     'personal medico': [2038, 1222, 1714, 2295],
+    'demanda': []
     'tiempo': [90, 115, 95, 80]
 })
 
 # Variables independientes
-X = datos[['camas', 'personal medico']]
+X = datos[['camas', 'personal medico', 'demanda']]
 
 # Variable dependiente
 y = datos[['tiempo']]
@@ -22,14 +23,33 @@ modelo.fit(X, y)
 # Obtenemos coeficientes y término de intercepción
 w1 = modelo.coef_[0][0]  # Peso de las camas
 w2 = modelo.coef_[0][1]  # Peso de los médicos
+w3 = modelo.coef_[0][2]  # Demanda
 intercepto = modelo.intercept_[0]
 
 # Imprimimos coeficientes y término de intercepción
-print("Coeficientes:\n \t w1=", w1, "\n \t w2=", w2)
-print("Término de intercepción:", intercepto)  #El intercepto representa el valor de y cuando todas las variables independientes son cero. 
-#En este contexto, podría interpretarse como el tiempo de respuesta esperado si un hospital no tuviera camas ni personal médico. 
-#Los coeficientes (w1 y w2) representan el cambio esperado en y por cada unidad de cambio en las variables independientes X1 (camas) y X2 (personal médico), respectivamente.
+print("Coeficientes:\n \t w1=", w1, "\n \t w2=", w2, '\n \t w3=', w3)
+print("Término de intercepción:", intercepto)  
 
+#PROGRAMACIÓN LINEAL. LIBRERÍA PULP.
+# Creamos el modelo de programación lineal
+modelo_lp = LpProblem("Tiempos de espera en función de camas, médicos y demanda", sense=LpMinimize)
 
+# Definimos las variables de decisión
+camas = LpVariable("Camas utilizadas", lowBound=0, cat="Integer")
+medicos = LpVariable("Médicos utilizados", lowBound=0, cat="Integer")
+demanda = LpVariable("Demanda", lowBound=0, cat="Integer")
+
+# Definimos la función objetivo
+modelo_lp += ( w1 * camas + w2 * medicos - w3 * demanda)
+
+# Restricciones. Nuestras restricciones están relacionadas con el presupuesto
+#modelo_lp += (3037 * camas + 2696 * medicos)<=presupuesto 
+
+# Resolvemos el modelo
+modelo_lp.solve()
+
+# Imprimimos los resultados
+print("\tEncontramos {} camas".format(int(camas.varValue)))
+print("\tEncontramos {} médicos".format(int(medicos.varValue)))
 
 
